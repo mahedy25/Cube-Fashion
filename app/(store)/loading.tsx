1a3b9124feb7bddd52c8130fe17b5b0d9dd483cf
mahedy25 <mahedyhasan873@gmail.com>
2025-11-21 19:31:57 +0600

    loader fixed

--- app/(store)/loading.tsx
+++ app/(store)/loading.tsx
@@ -2,7 +2,7 @@ import Loader from "../../components/Loader";
 
 export default function Loading() {
   return (
-    <div>
+    <div className='fixed inset-0 flex justify-center items-center bg-gray-100 z-[9999]'>
       <Loader />
     </div>
   )
--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,11 +1,11 @@
 export default function Loader() {
   return (
-    <div className='flex justify-center items-center h-screen bg-gray-100'>
+    <div className='flex justify-center items-center w-full min-h-svh bg-gray-100'>
       <div className='relative flex justify-center items-center'>
         {/* Outer spinning ring */}
-        <div className='absolute animate-spin rounded-full h-32 w-32 border-8 border-t-8 border-transparent border-r-[#D9004C] border-l-[#670626]'></div>
+        <div className='absolute animate-spin rounded-full h-32 w-32 border-8 border-transparent border-t-[#D9004C] border-l-[#670626]'></div>
 
-        {/* Inner glow effect */}
+        {/* Inner glow */}
         <div className='absolute animate-pulse rounded-full h-16 w-16 bg-[#D9004C] opacity-50'></div>
       </div>
     </div>
